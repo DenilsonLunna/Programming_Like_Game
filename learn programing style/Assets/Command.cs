415be29041a9d5d80b003730c6d8bb13ebe4e271
Denilson <denilsonsousa7@outlook.com>
2019-03-21 18:47:11 -0300

    Novo arquivo inserido
    Esse arquivo esta com os blocos e compilador funcionando e organizado

---
+++ learn programing style/Assets/Command.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+	public string name;
+	public Queue commandParams;
+
+	public void initiate ()
+	{
+		name = "";
+		commandParams = new Queue();
+	}
+}
---
+++ learn programing style/Assets/UI/Scripts/AcoesMenu/MenuAcoes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAcoes : MonoBehaviour {
+
+    [SerializeField] private GameObject programaBlocos;
+    [SerializeField] private GameObject menuPrincipal;
+    [SerializeField] private GameObject programaDesenho;
+    [SerializeField] private GameObject Web;
+
+    public void voltarParaMenu(){
+        programaBlocos.SetActive(false);
+        menuPrincipal.SetActive(true);
+        programaDesenho.SetActive(false);
+        Web.SetActive(false);
+    }
+    public void abrirProgramaBlocos(){
+        programaBlocos.SetActive(true);
+        menuPrincipal.SetActive(false);
+        programaDesenho.SetActive(false);
+        Web.SetActive(false);
+    }
+    public void abrirProgramaDesenho(){
+        programaBlocos.SetActive(false);
+        menuPrincipal.SetActive(false);
+        programaDesenho.SetActive(true);
+        Web.SetActive(false);
+    }
+    public void abrirProgramaWeb(){
+        programaBlocos.SetActive(false);
+        menuPrincipal.SetActive(false);
+        programaDesenho.SetActive(false);
+        Web.SetActive(true);
+    }
+}
